f433a305282cc3d38c169cc0db7c23a11ddf5ce8
zhangll <805502494@qq.com>
2023-12-01 11:00:54 +0800

    [feat]Add locale header directory

---
+++ c/b/test.h
@@ -0,0 +1,7 @@
+#ifndef __TEST__
+#define __TEST__
+
+#error "dir "b""
+
+#endif  /*__TEST__ */
+
--- c/main.c
+++ c/main.c
@@ -1,6 +1,7 @@
 //#include "hello.h"
 #include "stdio.h"
 #include "IPD.h"
+#include "test.h"
 
 
 int __attribute__((deprecated))deprecatedFn(void);
@@ -37,6 +38,8 @@ int main (void)
     strncpy(a1.name, "hei\0heiahglajlgja", 8);
     printf("%s %d\n", a1.name, a1.age);
 
-    printf("%d\n", ALIGN_DOWN_AS(1024, 4));
+    printf("%d %d\n",sizeof(long), ALIGN_DOWN_AS(1024, 4));
+    int aaa = 150;
+    printf("%lu.%lu\n",aaa/100, aaa%100);
     return 0;
 }
