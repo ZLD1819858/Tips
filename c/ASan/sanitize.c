d43382da2d8ae46cac9acbafa6c2a8cdf52c66d0
zhangll <805502494@qq.com>
2024-01-08 20:09:01 +0800

    [feat]Update note

--- c/ASan/sanitize.c
+++ c/ASan/sanitize.c
@@ -1,11 +1,31 @@
-#include "stdio.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/*
+    LLVM >= 3.1     GCC >= 4.9.2
+    -fsanitize=address (启动ASan)
+
+    -fno-omit-frame-pointer (让栈追溯信息更加友好)
+*/
 
 int main(int argc, char **argv) {
+/*
     int x[5] = {0};
     x[5] = 10;
     printf("%d \n", x[5]);
     printf("%d \n", x[6]);
-    return 0;
+*/
+
+    //int *p = NULL;
+    //*p = 0;
+
+    char *buffer = (char *)malloc(32);
+    memset(buffer, 1, 32);
+
+    free(buffer);
+
+    return EXIT_SUCCESS;
 }
 
 
