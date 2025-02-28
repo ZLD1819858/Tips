ec327e4cef6df7ff765b5cb1dfcc6423122278a8
zhangluodong <zhangluodong>
2025-02-28 21:14:05 +0800

    [feat]Arm PMSAv7.

--- c/pmsav7/include/pmsav7_bf.h
+++ c/pmsav7/include/pmsav7_bf.h
@@ -1,5 +1,16 @@
 #ifndef __PMSAV7_BF_H__
 #define __PMSAV7_BF_H__
 
+#define PMSAV7_AP_PRIV_NO_USER_NO       (0U)    /* 0b000 */
+#define PMSAV7_AP_PRIV_RW_USER_NO       (1U)    /* 0b001 */
+#define PMSAV7_AP_PRIV_RW_USER_RO       (2U)    /* 0b010 */
+#define PMSAV7_AP_PRIV_RW_USER_RW       (3U)    /* 0b011 */
+#define PMSAV7_AP_PRIV_UNP_USER_UNP     (4U)    /* 0b100 */
+#define PMSAV7_AP_PRIV_RO_USER_NO       (5U)    /* 0b101 */
+#define PMSAV7_AP_PRIV_RO_USER_RO       (6U)    /* 0b110 */
+#define PMSAV7_AP_PRIV_RO_USER_RO       (7U)    /* 0b111 */
+
+/* TEX S C B */
+#define PMSAV7_REGION_ATTR_
 
 #endif /* __PMSAV7_BF_H__ */
--- c/pmsav7/pmsav7.c
+++ c/pmsav7/pmsav7.c
@@ -3,4 +3,5 @@
 int main(int argc, char **argv)
 {
     __attribute__((unused))   PMSAV7_MPU_TYPE a;
+    printf("%d \n", PMSAV7_AP_PRIV_RW_USER_NO);
 }
